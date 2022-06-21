56ecc01ac4089771d57b122ffefdf1a9a5578a85
Prathibha Ponnuswamy <pprathi.01@gmail.com>
2022-06-21 18:42:42 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -1,21 +1,45 @@
 #include <stdio.h>
 #include <assert.h>
+#include "misaligned.h"
 
-int printColorMap() {
-    const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
-    const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
-    int i = 0, j = 0;
-    for(i = 0; i < 5; i++) {
-        for(j = 0; j < 5; j++) {
-            printf("%d | %s | %s\n", i * 5 + j, majorColor[i], minorColor[i]);
+const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
+const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+int numberOfMajorColors =  sizeof(majorColor) / sizeof(majorColor[0]);
+int numberOfMinorColors =  sizeof(minorColor) / sizeof(minorColor[0]);
+
+int printColorMap(void) {
+    int majorColorIndex = 0, minorColorIndex = 0, pairNumber = 0;
+    ColorPair colorPair;
+    for(majorColorIndex = 0; majorColorIndex < numberOfMajorColors; majorColorIndex++){
+        for(minorColorIndex = 0; minorColorIndex < numberOfMinorColors; minorColorIndex++){
+        	colorPair.majorColor = majorColorIndex;
+			colorPair.minorColor = minorColorIndex;
+			pairNumber = GetPairNumberFromColor(&colorPair);
+            printf("%-2d | %-6s | %s\n", pairNumber, majorColor[majorColorIndex], minorColor[minorColorIndex]);
         }
     }
-    return i * j;
+    return majorColorIndex * minorColorIndex;
 }
 
-int main() {
+int GetPairNumberFromColor(const ColorPair* colorPair)
+{
+    return colorPair->majorColor * numberOfMinorColors + colorPair->minorColor;
+}
+
+void testPairToNumber(MajorColor major, MinorColor minor, int expectedPairNumber)
+{
+    ColorPair colorPair;
+    colorPair.majorColor = major;
+    colorPair.minorColor = minor;
+    int pairNumber = GetPairNumberFromColor(&colorPair);
+    assert(pairNumber == expectedPairNumber);
+}
+
+int main(void) {
     int result = printColorMap();
     assert(result == 25);
+    testPairToNumber(BLACK, ORANGE, 12);
+    testPairToNumber(VIOLET, SLATE, 25);
     printf("All is well (maybe!)\n");
     return 0;
 }
